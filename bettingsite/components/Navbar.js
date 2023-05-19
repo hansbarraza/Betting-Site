efd99c481c767f9867f9f1fa5c1b08da8d59f82e
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-05-19 11:01:48 -0700

    created the navBar using  mutilple components and modals using mantine library to add login and create login

---
+++ bettingsite/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Top from 'components/Navbar/Top'
+import Middle from './Navbar/Middle'
+import Nav from './Navbar/Nav'
+export default function Navbar() {
+  return (
+    <div>
+       <Top/>
+       <Middle/>
+       <Nav/>
+    </div>
+  )
+}
---
+++ bettingsite/components/Navbar/Top.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AiFillCrown } from 'react-icons/ai'
+export default function Navbar() {
+  return (
+    <div className="bg-[#333333]">
+        <div className='text-white flex justify-end text-sm p-2'>
+            <div className='mt-1 mr-1'>
+            <AiFillCrown/>
+            </div>
+        Responsible Gambling
+        </div>
+    </div>
+  )
+}
